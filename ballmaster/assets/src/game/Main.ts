b8b369b8d723a72ab379a834b8a1a3d6e8d6f92e
e <l>
2020-04-26 11:28:39 +0800

    声音和自定义事件

--- ballmaster/assets/src/game/Main.ts
+++ ballmaster/assets/src/game/Main.ts
@@ -1,6 +1,7 @@
 import * as ui from "../common/ui/pop_mgr";
 import { TimerMgr } from "../common/timer/timer_mgr";
 import { EventDispatch, Event_Name } from "../common/event/EventDispatch"
+import * as AudioManager from "../common/audio/AudioPlayer"
 
 const { ccclass, property } = cc._decorator;
 
@@ -10,21 +11,24 @@ export default class NewClass extends cc.Component {
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
-        EventDispatch.ins().add(Event_Name.SHOW_TIPS, this.showTips, this);
-
+        /**自定义事件 */
+        EventDispatch.ins().add(Event_Name.TIPS_0, this.showTips, this);
+        AudioManager.AudioPlayer.ins().init();
     }
 
     private opentest1() {
+        AudioManager.AudioPlayer.ins().play_sound(AudioManager.AUDIO_CONFIG.Audio_Btn);
         ui.pop_mgr.get_inst().show(ui.UI_CONFIG.uitest_1);
     }
 
     private opentest2() {
+        AudioManager.AudioPlayer.ins().play_sound(AudioManager.AUDIO_CONFIG.Audio_Btn);
         ui.pop_mgr.get_inst().show(ui.UI_CONFIG.uitest_2);
     }
 
 
     private showTips(str: string = 'loading') {
-        console.log("自定义事件");
+        console.log("自定义事件处理逻辑", str);
     }
 
 
--- ballmaster/assets/src/game/view/uiTest_1.ts
+++ ballmaster/assets/src/game/view/uiTest_1.ts
@@ -1,6 +1,9 @@
 import { POP_UI_BASE } from "../../common/ui/pop_ui_base";
+import * as AudioManager from "../../common/audio/AudioPlayer"
+import { EventDispatch, Event_Name } from "../../common/event/EventDispatch"
 
 const { ccclass, property } = cc._decorator;
+
 
 @ccclass
 export default class NewClass extends POP_UI_BASE {
@@ -10,7 +13,20 @@ export default class NewClass extends POP_UI_BASE {
     // onLoad () {}
 
     start() {
+        EventDispatch.ins().fire(Event_Name.TIPS_0, `自定义参数${this.node.name}`);
+    }
 
+    on_show() {
+        AudioManager.AudioPlayer.ins().play_music(AudioManager.AUDIO_CONFIG.Audio_Bgm);
+    }
+
+    /**关闭界面时调用，用来做清理工作*/
+    on_hide(): void {
+        AudioManager.AudioPlayer.ins().stop_music();
+    }
+
+    hide() {
+        super.hide();
     }
 
     // update (dt) {}
